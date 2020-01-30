a2b4e238037238689f9d76f9a286325bc3c7e35b
haack79 <haack@Brians-MacBook-Pro-3.local>
2020-01-29 21:13:06 -0800

    updated this discussion

--- es6/arrow.js
+++ es6/arrow.js
@@ -24,3 +24,21 @@ ages6 = years.map((el, index) => {
 
 console.log(ages6); 
 
+// arrow functions share the this key word, they don't have a this, they just use the this of
+// the function they are in. 
+
+//es5 
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function() { // this is a method and has access to the this of the object
+        document.querySelector('.green').addEventListener('click', function() {// this eventhandler and callback is a regular function call and so points to the window object. 
+            var str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str); 
+        });
+    }
+}
+// box5.clickMe(); // This is box number undefined and it is undefined. 
+// the reason is cause the 'this' keyword is pointing to the global object - in the case of the
+// browser it is the window object and not the box5 object. 
+// only in a method call the this keyword will point to the object but not in a function call
