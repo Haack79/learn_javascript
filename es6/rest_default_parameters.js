3d08c07103bc8f56e969ffc588b119542260e6d1
haack79 <haack@Brians-MBP-3.localdomain>
2020-01-31 08:30:42 -0800

    added rest and default

---
+++ es6/rest_default_parameters.js
@@ -0,0 +1,44 @@
+// rest parameters allow you to pass in an arbitrary number of arguments into a function 
+// and to use these arguments in that function
+// rest paramaters take single values and transforms them into an array
+// this is opposite spread which takes array of values and puts them into an array
+//**************** rest param */
+//ES5
+function isFullAge() {
+    console.log(arguments); // special variable have access to in all functions. 
+}
+
+isFullAge(2,3,1); // [2, 3, 1]; 
+
+function isFullAges5() {
+    var argsArr = Array.prototype.slice.call(arguments); 
+    argsArr.forEach(function(cur) {
+        console.log((2020 - cur) >= 18); 
+    });
+}
+isFullAges5(1990, 2001, 2003); 
+
+function isFullAge6(...years) {
+    console.log(years);
+}
+isFullAge6(1900,2001, 1999,2008); // prints avlues out as an array [1990, 2001, etc]
+
+isFullAge67(...years) {
+    years.forEach(cur => console.log((2020 - cur) >= 18)); 
+}
+// ************* now call with a limit parameter. ************************
+// ES5 --------------------
+function isFullAges5Limit(limit) {
+    var argsArr = Array.prototype.slice.call(arguments, 1); // cause of slice it starts copying the array at 1.// meaning we exclude the first argument
+    argsArr.forEach(function(cur) {
+        console.log((2020 - cur) >= limit); 
+    });
+}
+isFullAges5Limit(21, 1990, 2001, 2003); 
+// ES6 ******* ------------
+
+function isFullAge67Limit(limit, ...years) {
+    years.forEach(cur => console.log((2020 - cur) >= limit)); 
+}
+isFullAge67Limit(100, 1900,2001, 1999,2008);
+
