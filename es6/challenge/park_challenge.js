90dce73c0c7cfc02409e5bc5a53b9428cdccc685
haack79 <haack@Brians-MacBook-Pro-3.local>
2020-02-01 22:13:44 -0800

    adding challenge

---
+++ es6/challenge/park_challenge.js
@@ -0,0 +1,8 @@
+// parks and streets have name and build year
+// method trees/ parks area
+// avg age of parks = sum of all park ages / # of parks
+// name of park that has over 1000 trees
+// total and avg length of parks streets
+// classification of streets tiny, small, normal, big, huge, if size unknown then normal is default
+// print to console
+clas
